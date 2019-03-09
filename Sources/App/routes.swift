80902f737157851733b83e3502db3e0dc2aad42b
LukeJ <infiLuca@gmail.com>
2019-03-09 19:27:39 +0100

    refactor review routes into separate controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,6 +7,9 @@ public func routes(_ router: Router) throws {
     let coursesController = CoursesController()
     try router.register(collection: coursesController)
 
+    let reviewController = ReviewController()
+    try router.register(collection: reviewController)
+
 
     // Basic "It works" example
     router.get { req in
@@ -20,94 +23,6 @@ public func routes(_ router: Router) throws {
 
 
 
-    // POST /api/courses/:courseId/reviews
-    // Create a new review for a course
-    // Review info in JSON body
-    router.post("api", "courses", Course.parameter, "reviews") { req -> Future<Review> in
-        return try flatMap(
-            to: Review.self,
-            req.parameters.next(Course.self),
-            req.content.decode(Review.self)) { course, review in
-
-                guard course.id == review.courseID else {
-                    throw Abort(.badRequest, reason: "Course ID specified in JSON does not match")
-                }
-
-                return review.save(on: req)
-            }
-    }
-
-    // GET /api/courses/:courseId/reviews
-    // return all reviews for a course
-    router.get("api", "courses", Course.parameter, "reviews") { req -> Future<[Review]> in
-        return try req.parameters.next(Course.self).flatMap(to: [Review].self) { course in
-            try course.reviews.query(on: req).all()
-        }
-    }
-
-
-    // GET /api/courses/:courseId/reviews/:reviewId
-    // return a single review
-    router.get("api", "courses", Course.parameter, "review", Int.parameter) { req -> Future<Review> in
-        return try req.parameters.next(Course.self).flatMap(to: Review.self) { course in
-            let reviewId = try req.parameters.next(Int.self)
-            return try course.reviews.query(on: req)
-                    .filter(\Review.id == reviewId)
-                    .first()
-                    .map(to: Review.self) { filteredReview in
-
-                        guard let filteredReview = filteredReview else {
-                            throw Abort(.notFound, reason: "No review available for specified ID");
-                        }
-
-                        return filteredReview
-            }
-        }
-    }
-
-    // PUT /api/courses/:courseId/reviews/:reviewId
-    // update a review
-    router.put("api", "courses", Course.parameter, "reviews", Int.parameter) { req -> Future<Review> in
-        return try flatMap(
-                to: Review.self,
-                req.parameters.next(Course.self),
-                req.content.decode(Review.self)) { course, updatedReview in
-            let reviewId = try req.parameters.next(Int.self)
-            return try course.reviews.query(on: req)
-                    .filter(\Review.id == reviewId)
-                    .first()
-                    .flatMap(to: Review.self) { filteredReview in
-
-                        guard let filteredReview = filteredReview else {
-                            throw Abort(.notFound, reason: "No review available for specified ID");
-                        }
-
-                        filteredReview.rating = updatedReview.rating
-                        filteredReview.comment = updatedReview.comment
-
-                        return filteredReview.save(on: req)
-            }
-        }
-    }
-
-    // DELETE /api/courses/:courseId/reviews/:reviewId
-    // Delete a review
-    router.delete("api", "courses", Course.parameter, "reviews", Int.parameter) { req -> Future<HTTPStatus> in
-        return try req.parameters.next(Course.self).flatMap(to: HTTPStatus.self) { course in
-            let reviewId = try req.parameters.next(Int.self)
-            return try course.reviews.query(on: req)
-                    .filter(\Review.id == reviewId)
-                    .first()
-                    .flatMap(to: HTTPStatus.self) { filteredReview in
-
-                        guard let filteredReview = filteredReview else {
-                            throw Abort(.notFound, reason: "No review available for specified ID");
-                        }
-
-                        return filteredReview.delete(on: req).transform(to: HTTPStatus.noContent)
-            }
-        }
-    }
 
 
 
